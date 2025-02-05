23bb98ee30b5ebc50d082c8cdaa6d498ddd98371
Verseler <vrslr2003@gmail.com>
2025-02-05 23:12:36 +0800

    fix: vuetify icon issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,14 @@
+import '@mdi/font/css/materialdesignicons.css';
 import '../css/app.css';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { VFileUpload } from 'vuetify/labs/VFileUpload';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -16,8 +21,17 @@ createInertiaApp({
             import.meta.glob('./Pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
+        const vuetify = createVuetify({
+            components: {
+                ...components,
+                VFileUpload,
+            },
+            directives,
+        });
+
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(vuetify)
             .use(ZiggyVue)
             .mount(el);
     },
