ce3e8a3d8ebaf459035e3709e1691ea5f716f3b6
Verseler <vrslr2003@gmail.com>
2025-02-06 16:35:50 +0800

    feat: sidebar with initial routing for auth users

---
+++ resources/js/Components/SideBar.vue
@@ -0,0 +1,90 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import NavLink from '@/Components/NavLink.vue';
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    show: Boolean,
+});
+</script>
+
+<template>
+    <!-- Primary Navigation Menu -->
+    <div
+        :class="{ hidden: !show, flex: show }"
+        class="fixed min-h-screen w-60 flex-col justify-between border-e shadow-sm sm:w-80"
+    >
+        <nav class="p-4">
+            <!-- Logo -->
+            <Link :href="route('dashboard')">
+                <ApplicationLogo class="mx-auto mb-8 mt-4 h-12" />
+            </Link>
+
+            <v-divider></v-divider>
+
+            <!-- Navigation Links -->
+            <v-list class="flex flex-col gap-y-1">
+                <NavLink
+                    :href="route('dashboard')"
+                    :active="route().current('dashboard')"
+                    prepend-icon="mdi-view-dashboard-outline"
+                >
+                    Dashboard
+                </NavLink>
+                <NavLink
+                    :href="route('document.owned')"
+                    :active="route().current('document.owned')"
+                    prepend-icon="mdi-folder-account-outline"
+                >
+                    My Documents
+                </NavLink>
+                <NavLink
+                    :href="route('document.shared')"
+                    :active="route().current('document.shared')"
+                    prepend-icon="mdi-folder-arrow-right-outline"
+                >
+                    Shared Documents
+                </NavLink>
+                <NavLink
+                    :href="route('document.trash')"
+                    :active="route().current('document.trash')"
+                    prepend-icon="mdi-delete-outline"
+                >
+                    Trash
+                </NavLink>
+            </v-list>
+        </nav>
+
+        <!-- User Profile Dropdown -->
+        <div class="flex items-center gap-x-2.5 border-t p-2">
+            <Link
+                :href="route('profile.edit')"
+                class="flex w-full cursor-pointer items-center gap-x-2.5 text-start transition-transform active:scale-95"
+            >
+                <v-avatar
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIEd2zxEc_4IQ1jHyniHLECu15zRjkHTBJzA&s"
+                ></v-avatar>
+                <!-- user name and role -->
+                <div>
+                    <p class="leading-5 text-neutral-900">
+                        {{ $page.props.auth.user.name }}
+                    </p>
+                    <p class="text-sm leading-none text-neutral-500">Admin</p>
+                </div>
+            </Link>
+
+            <!-- Logout Button -->
+            <Link
+                :href="route('logout')"
+                method="post"
+                as="button"
+                type="button"
+            >
+                <v-icon
+                    icon="mdi-logout"
+                    class="px-4 opacity-70 transition-all hover:opacity-100 active:text-red-500"
+                />
+            </Link>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/SharedDocuments.vue
+++ resources/js/Pages/SharedDocuments.vue
@@ -4,23 +4,15 @@ import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head title="Shared Documents" />
 
     <AuthenticatedLayout>
         <template #header>
             <div class="w-full">
                 <h2 class="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard
+                    Shared Documents
                 </h2>
             </div>
         </template>
-
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
-            </div>
-        </div>
     </AuthenticatedLayout>
 </template>
